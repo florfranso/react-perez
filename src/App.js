e0f4b7414c2b465db7a6e29ea2ffeac50b11e7ab
flor perez <flor.franso@live.com>
2022-08-25 23:54:06 -0300

    cambie titutlo pagina

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TRABAJO <code>FLORENCIA PEREZ</code> React
         </p>
         <a
           className="App-link"
