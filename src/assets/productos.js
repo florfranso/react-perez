5f966d41d9f59173d3b4ea8d71cf1b86cff6da50
flor perez <flor.franso@live.com>
2022-09-13 19:39:30 -0300

    agregue url img

--- src/assets/productos.js
+++ src/assets/productos.js
@@ -1,29 +1,29 @@
 export const products = [
     {
         id:1,
-        image: "../../public/imagenes/piramidal-25.jpg",
+        image: "https://res.cloudinary.com/reactflor/image/upload/v1663108484/piramidal-25_zvmwed.jpg",
         product: "PIRAMIDAL 25",
         categoria: "MACETAS",
         descripcion: "",
-        precio: 2,
+        precio: 2250,
         stock: 2,
 
     },
 
     {
         id:2,
-        image: " ",
+        image: "https://res.cloudinary.com/reactflor/image/upload/v1663108491/sansevieria_o63mmn.jpg",
         product: "SANSIVERIA",
         categoria: "PLANTAS DE INTERIOR",
         descripcion: "",
-        precio: 55,
+        precio: 1200,
         stock: 5
 
     },
 
     {
         id:3,
-        image: "",
+        image: "https://res.cloudinary.com/reactflor/image/upload/v1663108264/agapanto-azul_k9mbtt.jpg",
         product: "AGAPANTOS AZUL",
         categoria:"PLANTAS DE EXTERIOR",
         descripcion: "",
@@ -33,21 +33,21 @@ export const products = [
 
     {
         id:4,
-        image: "",
+        image: "https://res.cloudinary.com/reactflor/image/upload/v1663108468/humuslombriz_gdngqy.jpg",
         product: "HUMUS DE LOMBRIZ 3dm",
         categoria: "SUSTRATOS Y MEJORADORES",
         descripcion:"",
-        precio:5,
+        precio:380,
         stock: 6
     },
 
     {
         id:5,
-        image: "../../public/imagenes/Juego jardin 3 piezas mango madera (1).jpg",
+        image: "https://res.cloudinary.com/reactflor/image/upload/v1663108475/Juego_jardin_3_piezas_mango_madera_1_vtopyc.jpg",
         product: "SET JARDINERIA x3",
         categoria: "ACCESORIOS DE JADIN",
         descripcion: "",
-        precio:5,
+        precio:650,
         stock:5
     },
 ]
